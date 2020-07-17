68a8d4626c70e400a43d3a6aa40cc27e827ad154
Radu <rsrusu90@gmail.com>
2020-07-17 11:38:00 +0100

    adding contact me and footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,29 +134,29 @@ var U3 = document.getElementById("U3")
 
 }
 
-$(document).scroll(function() {
-  console.log($(document).scrollTop());
-})
+// $(document).scroll(function() {
+//   console.log($(document).scrollTop());
+// })
 
-  $(document).ready(function() {
+  // $(document).ready(function() {
     
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
+  //   /* Every time the window is scrolled ... */
+  //   $(window).scroll( function(){
 
 
-        /* Check the location of each desired element */
-        $('.fade-in').each( function(i){          
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
+  //       /* Check the location of each desired element */
+  //       $('.fade-in').each( function(i){          
+  //           var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+  //           var bottom_of_window = $(window).scrollTop() + $(window).height();
              
             
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-                $(this).animate({'opacity':'1'} ,300);
-            }
-        });  
-    }); 
-  });
+  //           /* If the object is completely visible in the window, fade it it */
+  //           if( bottom_of_window > bottom_of_object ){
+  //               $(this).animate({'opacity':'1'} ,300);
+  //           }
+  //       });  
+  //   }); 
+  // });
 
   function Circle(el){
     $(el).circleProgress({fill: {color: '#2385a6'}})
@@ -171,6 +171,8 @@ $(document).scroll(function() {
       Circle('.round2');
       Circle('.round3');
       Circle('.round4');
+      Circle('.round5');
+      Circle('.round6');
   }
 
 
